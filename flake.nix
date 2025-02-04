48df774c20719afae1cac65469affe00a87d746f
ecyht2 <ecyht2@nottingham.edu.my>
2025-02-04 13:38:08 +0800

    feat: Added flake dev shell

--- flake.nix
+++ flake.nix
@@ -13,7 +13,13 @@
     allSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     # A function that provides a system-specific Nixpkgs for the desired systems
     forAllSystems = nixpkgs.lib.genAttrs allSystems;
+    forAllPkgs = f:
+      forAllSystems (system:
+        f {
+          pkgs = import nixpkgs {system = system;};
+        });
   in {
     formatter = forAllSystems (system: nixpkgs.legacyPackages."${system}".alejandra);
+    devShell = forAllPkgs ({ pkgs }: pkgs.callPackage ./shell.nix {});
   };
 }
