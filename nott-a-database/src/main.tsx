00a71570527c9fd6d3de678c9ddaf07933af1f81
ecyht2 <ecyht2@nottingham.edu.my>
2025-02-16 22:25:38 +0800

    feat: Added dark theme

--- nott-a-database/src/main.tsx
+++ nott-a-database/src/main.tsx
@@ -10,49 +10,54 @@ import StudentsPage from "./students";
 import ModulesPage from "./modules";
 import StudentInfo from "./student_info";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <nav className="border-b p-4">
-        <div className="container mx-auto flex items-center justify-between">
-          <h1 className="text-xl font-bold">
-            <Link to="/">Nott A Database</Link>
-          </h1>
-          <ul className="flex gap-x-4">
-            <li>
-              <Link to="/" className="hover:text-gray-300">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/upload" className="hover:text-gray-300">
-                Upload
-              </Link>
-            </li>
-            <li>
-              <Link to="/students" className="hover:text-gray-300">
-                Students
-              </Link>
-            </li>
-            <li>
-              <Link to="/modules" className="hover:text-gray-300">
-                Modules
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <main className="m-1">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="upload" element={<UploadPage />} />
-          <Route path="students" element={<StudentsPage />} />
-          <Route path="student" element={<StudentInfo />} />
-          <Route path="modules" element={<ModulesPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-    <Toaster />
+    <ThemeProvider>
+      <BrowserRouter>
+        <nav className="border-b p-4">
+          <div className="container mx-auto flex items-center gap-x-4">
+            <h1 className="text-xl font-bold">
+              <Link to="/">Nott A Database</Link>
+            </h1>
+            <ul className="ml-auto flex gap-x-4">
+              <li>
+                <Link to="/" className="hover:text-gray-300">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/upload" className="hover:text-gray-300">
+                  Upload
+                </Link>
+              </li>
+              <li>
+                <Link to="/students" className="hover:text-gray-300">
+                  Students
+                </Link>
+              </li>
+              <li>
+                <Link to="/modules" className="hover:text-gray-300">
+                  Modules
+                </Link>
+              </li>
+            </ul>
+            <ModeToggle />
+          </div>
+        </nav>
+        <main className="m-1">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="upload" element={<UploadPage />} />
+            <Route path="students" element={<StudentsPage />} />
+            <Route path="student" element={<StudentInfo />} />
+            <Route path="modules" element={<ModulesPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>,
 );
