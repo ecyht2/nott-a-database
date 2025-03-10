d00ab74a13f2b39897ec217efc3d7ef000cae228
ecyht2 <ecyht2@nottingham.edu.my>
2025-03-10 13:43:00 +0800

    fix: Fix page count in Students page

--- nott-a-database/src/students.tsx
+++ nott-a-database/src/students.tsx
@@ -232,8 +232,8 @@ export default function StudentsPage() {
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+          Page {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount()} page(s).
         </div>
         <div className="space-x-2">
           <Button
